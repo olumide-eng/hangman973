64e619e7e87ab171663b141ed9442cd91f7d9d00
olumide <olumide@olumides-MacBook-Pro-2.local>
2024-03-14 09:01:22 +0000

    Readme-updated

---
+++ milestone_2.py
@@ -0,0 +1,18 @@
+"""
+Step 1:
+Create a list containing the names of your 5 favorite fruits.
+
+
+Step 2:
+Assign this list to a variable called word_list.
+
+
+Step 3:
+Print out the newly created list to the standard output (screen).
+
+
+"""
+
+
+word_list= ["apple", "banana", "orange", "grape", "kiwi"] 
+print(word_list) 
