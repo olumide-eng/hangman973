4a88881a6425fa0001c6d0a8c3a21496d4088021
olumide <iloolumide@gmail.com>
2024-03-14 09:44:26 +0000

    regularized-username

--- milestone_2.py
+++ milestone_2.py
@@ -5,20 +5,16 @@ word_list= ["apple", "banana", "orange", "grape", "kiwi"]
 
 word = random.choice(word_list)
 guess = input("Enter a single letter: " )
+
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
 print(word_list) 
 print(word)
 
 
 '''
-In this task, you are required to take user input. As you now know, the print() function in Python displays output on the screen. Conversely
-, Python has an input() function that takes input from the screen. Note that the input function returns the user input in form of a string.
 
-
-Step 1:
-Using the input function, ask the user to enter a single letter.
-
-
-Step 2:
-Assign the input to a variable called guess.
 
 '''
