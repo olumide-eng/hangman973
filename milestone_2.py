a454bc2a46b82c6855e889c11ed99a923c331fb4
olumide <olumide@olumides-MacBook-Pro-2.local>
2024-03-14 09:12:28 +0000

    task-3

--- milestone_2.py
+++ milestone_2.py
@@ -1,18 +1,24 @@
-"""
-Step 1:
-Create a list containing the names of your 5 favorite fruits.
-
-
-Step 2:
-Assign this list to a variable called word_list.
-
-
-Step 3:
-Print out the newly created list to the standard output (screen).
-
-
-"""
+import random
 
 
 word_list= ["apple", "banana", "orange", "grape", "kiwi"] 
+
+word = random.choice(word_list)
+guess = input("Enter a single letter: " )
 print(word_list) 
+print(word)
+
+
+'''
+In this task, you are required to take user input. As you now know, the print() function in Python displays output on the screen. Conversely
+, Python has an input() function that takes input from the screen. Note that the input function returns the user input in form of a string.
+
+
+Step 1:
+Using the input function, ask the user to enter a single letter.
+
+
+Step 2:
+Assign the input to a variable called guess.
+
+'''
